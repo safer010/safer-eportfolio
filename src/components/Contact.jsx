15a30d7a18b40b29cade92a6a88c7cb96232975b
Safer Marie Pattawi <safermarie10@gmail.com>
2024-02-10 02:17:20 +0800

    added git logo and link my git account

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,6 +10,7 @@ import ContactPaperbg2 from '../assets/paperbag6.5.png'
 import ContactFlower1 from '../assets/flower10.png'
 import ContactButterfly1 from '../assets/butterfly2.png'
 import ContactDrawing1 from '../assets/drawing9.png'
+import Gitlogo from '../assets/gitlogo.png'
 
 
 
@@ -29,6 +30,7 @@ const Contact = () => {
         <h1 className='Contact-h1'>Contact</h1>
         <p className='Contact-p'>Feel free to reach out if you want a to build something together, have a question, or just want to connect.</p>
         <p className='Contact-p-link'><a href="mailto:safermarie10@gmail.com" className='Contact-link'>safermarie10@gmail.com</a></p>
+        <p className='Contact-git-link'><Link to="https://github.com/safer010" target='blank'><img src={Gitlogo} alt="" className='Contact-git-logo' /></Link></p>
 
         <img src={ContactPaperbg2} alt="" className='ContactPaperbg2'/>
 
